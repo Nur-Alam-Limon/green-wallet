0db4cc3186ec37de8e208e713e4ee9ba2ff18a24
Nur-Alam-Limon <nuralam201425@gmail.com>
2024-01-23 16:21:14 +0600

    Showing discount on frontend

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,16 +1,78 @@
-import { useEffect } from "react";
-import { useActionData } from "@remix-run/react";
-import { Page, Card, Layout, Text } from "@shopify/polaris";
+import { useEffect, useState } from "react";
+import { json, useLoaderData } from "@remix-run/react";
+import { Page, Card, Layout, Text, DataTable } from "@shopify/polaris";
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }) => {
-  await authenticate.admin(request);
+  const { admin } = await authenticate.admin(request);
 
-  return null;
+  const response = await admin.graphql(
+    `query ($namespace: String!, $key: String!) {
+        shop {
+        metafield(namespace: $namespace, key: $key) {
+          value
+        }
+      }
+      }`,
+    {
+      variables: {
+        namespace: "green-wallet",
+          key: "discount",
+      },
+    }
+  );
+  const responseJson = await response.json();
+  console.log("Response JSON", responseJson);
+  const data = responseJson.data.shop;
+  console.log("Data", data);
+  return json({ data });
 };
 
 export default function Index() {
+  const loaderData = useLoaderData();
 
+  const [discountRules, setdiscountRules] = useState();
+
+  useEffect(() => {
+    if (loaderData?.data?.metafield?.value === undefined) {
+      console.log(loaderData, "loaderData is undefined");
+    } else {
+      const existingRulesData = JSON.parse(
+        loaderData?.data?.metafield?.value
+      );
+      console.log("Loader", existingRulesData);
+
+      setdiscountRules(existingRulesData);
+      // console.log(existingRulesData?.rules);
+    }
+  }, [loaderData]);
+
+  return (<ExistingRules discountRules={discountRules} />);
+}
+
+function DiscountRulesTable ({ data }) {
+  const rows = [];
+  data?.forEach((item) => {
+    const MinToken=item.tokenQuantity;
+    const discountCode=item.discountCode;
+    
+
+    rows.push([MinToken, discountCode]);
+  });
+
+  return (
+    <DataTable
+      columnContentTypes={["text", "text"]}
+      headings={[
+        "Min Token Quantity",
+        "Discount Code",
+      ]}
+      rows={rows}
+    />
+  );
+}
+
+function ExistingRules({ discountRules }) {
   return (
     <Page
       title="Green Wallet App"
@@ -27,18 +89,18 @@ export default function Index() {
       <Layout>
         <Layout.Section>
           <Card>
-            {/* {upsellRules?.length > 0 ? (
+            {discountRules?.length > 0 ? (
                 <>
                   <Text as="h2" variant="headingMd">
                     Existing Upsell Rules
                   </Text>
-                  <UpsellRulesTable data={upsellRules} />
+                  <DiscountRulesTable data={discountRules} />
                 </>
-              ) : ( */}
+              ) : (
             <Text as="h2" variant="headingMd">
               No Discount rules Found!
             </Text>
-            {/* )} */}
+            )}
           </Card>
         </Layout.Section>
       </Layout>
