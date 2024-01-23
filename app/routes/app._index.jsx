a3fda3db5c8ae1ea577f9149476b19d07ec74eb7
Nur-Alam-Limon <nuralam201425@gmail.com>
2024-01-23 15:12:55 +0600

    Created basic functionality for discount creation and metafield

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,19 +1,14 @@
 import { useEffect } from "react";
 import { json } from "@remix-run/node";
-import { useActionData, useNavigation, useSubmit } from "@remix-run/react";
+import { useActionData, useSubmit } from "@remix-run/react";
 import {
   Page,
-  Layout,
-  Text,
   Card,
-  Button,
-  BlockStack,
-  Box,
-  List,
-  Link,
-  InlineStack,
+  Layout,
+  Text
 } from "@shopify/polaris";
 import { authenticate } from "../shopify.server";
+import axios from "axios";
 
 export const loader = async ({ request }) => {
   await authenticate.admin(request);
@@ -22,259 +17,157 @@ export const loader = async ({ request }) => {
 };
 
 export const action = async ({ request }) => {
-  const { admin } = await authenticate.admin(request);
-  const color = ["Red", "Orange", "Yellow", "Green"][
-    Math.floor(Math.random() * 4)
-  ];
+  const { admin, session } = await authenticate.admin(request);
+
+  console.log("Session", session);
+
   const response = await admin.graphql(
     `#graphql
-      mutation populateProduct($input: ProductInput!) {
-        productCreate(input: $input) {
-          product {
-            id
-            title
-            handle
-            status
-            variants(first: 10) {
-              edges {
-                node {
-                  id
-                  price
-                  barcode
-                  createdAt
+    mutation discountCodeBasicCreate($basicCodeDiscount: DiscountCodeBasicInput!) {
+      discountCodeBasicCreate(basicCodeDiscount: $basicCodeDiscount) {
+        codeDiscountNode {
+          codeDiscount {
+            ... on DiscountCodeBasic {
+              title
+              codes(first: 10) {
+                nodes {
+                  code
                 }
               }
+              startsAt
+              endsAt
+              customerSelection {
+                ... on DiscountCustomerAll {
+                  allCustomers
+                }
+              }
+              customerGets {
+                value {
+                  ... on DiscountPercentage {
+                    percentage
+                  }
+                }
+                items {
+                  ... on AllDiscountItems {
+                    allItems
+                  }
+                }
+              }
+              appliesOncePerCustomer
             }
           }
         }
-      }`,
+        userErrors {
+          field
+          code
+          message
+        }
+      }
+    }`,
     {
       variables: {
-        input: {
-          title: `${color} Snowboard`,
-          variants: [{ price: Math.random() * 100 }],
-        },
+        "basicCodeDiscount": {
+          "title": "17% off all items",
+          "code": "DISCOUNT17",
+          "startsAt": new Date().toISOString().split('T')[0] + 'T00:00:00Z',
+          "customerSelection": {
+            "all": true
+          },
+          "customerGets": {
+            "value": {
+              "percentage": 0.2
+            },
+            "items": {
+              "all": true
+            }
+          },
+          "appliesOncePerCustomer": false
+        }
       },
-    }
+    },
   );
+
   const responseJson = await response.json();
 
+  console.log('response json', responseJson?.data?.discountCodeBasicCreate);
+
+  if(responseJson?.data?.discountCodeBasicCreate?.codeDiscountNode?.codeDiscount){
+
+    const dataNew = await axios.post(
+      `https://${session.shop}/admin/api/2024-01/metafields.json`,
+      {
+        metafield: {
+          namespace: "green-wallet",
+          key: "discount",
+          value: JSON.stringify(responseJson?.data?.discountCodeBasicCreate?.codeDiscountNode?.codeDiscount),
+          type: "json",
+        },
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "X-Shopify-Access-Token": session?.accessToken,
+          "Accept-Encoding": "gzip,deflate,compress",
+        },
+      }
+    );
+
+    console.log("Metafield Data", dataNew.data);
+  }
+
   return json({
-    product: responseJson.data?.productCreate?.product,
+    discount: responseJson?.data?.discountCodeBasicCreate,
   });
 };
 
 export default function Index() {
-  const nav = useNavigation();
   const actionData = useActionData();
   const submit = useSubmit();
-  const isLoading =
-    ["loading", "submitting"].includes(nav.state) && nav.formMethod === "POST";
-  const productId = actionData?.product?.id.replace(
-    "gid://shopify/Product/",
-    ""
-  );
+  const discountCreate = actionData?.discount;
+
 
   useEffect(() => {
-    if (productId) {
-      shopify.toast.show("Product created");
+    if (discountCreate?.codeDiscountNode) {
+      shopify.toast.show("Discount created");
+    } else if(discountCreate?.userErrors){
+      shopify.toast.show(`Error: ${discountCreate?.userErrors[0]?.message}`);
     }
-  }, [productId]);
+  }, [discountCreate]);
   const generateProduct = () => submit({}, { replace: true, method: "POST" });
 
   return (
-    <Page>
-      <ui-title-bar title="Remix app template">
-        <button variant="primary" onClick={generateProduct}>
-          Generate a product
-        </button>
-      </ui-title-bar>
-      <BlockStack gap="500">
-        <Layout>
-          <Layout.Section>
-            <Card>
-              <BlockStack gap="500">
-                <BlockStack gap="200">
+    <Page
+      title="Green Wallet App"
+      primaryAction={{
+        content: "Add New Discount",
+        url: "/app/discount-config",
+      }}
+      // secondaryActions={
+      //   <Button variant="primary" url="/app/discount-edit">
+      //     Delete Existing
+      //   </Button>
+      // }
+    >
+      <Layout>
+        <Layout.Section>
+          <Card>
+            
+              {/* {upsellRules?.length > 0 ? (
+                <>
                   <Text as="h2" variant="headingMd">
-                    Congrats on creating a new Shopify app 🎉
+                    Existing Upsell Rules
                   </Text>
-                  <Text variant="bodyMd" as="p">
-                    This embedded app template uses{" "}
-                    <Link
-                      url="https://shopify.dev/docs/apps/tools/app-bridge"
-                      target="_blank"
-                      removeUnderline
-                    >
-                      App Bridge
-                    </Link>{" "}
-                    interface examples like an{" "}
-                    <Link url="/app/additional" removeUnderline>
-                      additional page in the app nav
-                    </Link>
-                    , as well as an{" "}
-                    <Link
-                      url="https://shopify.dev/docs/api/admin-graphql"
-                      target="_blank"
-                      removeUnderline
-                    >
-                      Admin GraphQL
-                    </Link>{" "}
-                    mutation demo, to provide a starting point for app
-                    development.
-                  </Text>
-                </BlockStack>
-                <BlockStack gap="200">
-                  <Text as="h3" variant="headingMd">
-                    Get started with products
-                  </Text>
-                  <Text as="p" variant="bodyMd">
-                    Generate a product with GraphQL and get the JSON output for
-                    that product. Learn more about the{" "}
-                    <Link
-                      url="https://shopify.dev/docs/api/admin-graphql/latest/mutations/productCreate"
-                      target="_blank"
-                      removeUnderline
-                    >
-                      productCreate
-                    </Link>{" "}
-                    mutation in our API references.
-                  </Text>
-                </BlockStack>
-                <InlineStack gap="300">
-                  <Button loading={isLoading} onClick={generateProduct}>
-                    Generate a product
-                  </Button>
-                  {actionData?.product && (
-                    <Button
-                      url={`shopify:admin/products/${productId}`}
-                      target="_blank"
-                      variant="plain"
-                    >
-                      View product
-                    </Button>
-                  )}
-                </InlineStack>
-                {actionData?.product && (
-                  <Box
-                    padding="400"
-                    background="bg-surface-active"
-                    borderWidth="025"
-                    borderRadius="200"
-                    borderColor="border"
-                    overflowX="scroll"
-                  >
-                    <pre style={{ margin: 0 }}>
-                      <code>{JSON.stringify(actionData.product, null, 2)}</code>
-                    </pre>
-                  </Box>
-                )}
-              </BlockStack>
-            </Card>
-          </Layout.Section>
-          <Layout.Section variant="oneThird">
-            <BlockStack gap="500">
-              <Card>
-                <BlockStack gap="200">
-                  <Text as="h2" variant="headingMd">
-                    App template specs
-                  </Text>
-                  <BlockStack gap="200">
-                    <InlineStack align="space-between">
-                      <Text as="span" variant="bodyMd">
-                        Framework
-                      </Text>
-                      <Link
-                        url="https://remix.run"
-                        target="_blank"
-                        removeUnderline
-                      >
-                        Remix
-                      </Link>
-                    </InlineStack>
-                    <InlineStack align="space-between">
-                      <Text as="span" variant="bodyMd">
-                        Database
-                      </Text>
-                      <Link
-                        url="https://www.prisma.io/"
-                        target="_blank"
-                        removeUnderline
-                      >
-                        Prisma
-                      </Link>
-                    </InlineStack>
-                    <InlineStack align="space-between">
-                      <Text as="span" variant="bodyMd">
-                        Interface
-                      </Text>
-                      <span>
-                        <Link
-                          url="https://polaris.shopify.com"
-                          target="_blank"
-                          removeUnderline
-                        >
-                          Polaris
-                        </Link>
-                        {", "}
-                        <Link
-                          url="https://shopify.dev/docs/apps/tools/app-bridge"
-                          target="_blank"
-                          removeUnderline
-                        >
-                          App Bridge
-                        </Link>
-                      </span>
-                    </InlineStack>
-                    <InlineStack align="space-between">
-                      <Text as="span" variant="bodyMd">
-                        API
-                      </Text>
-                      <Link
-                        url="https://shopify.dev/docs/api/admin-graphql"
-                        target="_blank"
-                        removeUnderline
-                      >
-                        GraphQL API
-                      </Link>
-                    </InlineStack>
-                  </BlockStack>
-                </BlockStack>
-              </Card>
-              <Card>
-                <BlockStack gap="200">
-                  <Text as="h2" variant="headingMd">
-                    Next steps
-                  </Text>
-                  <List>
-                    <List.Item>
-                      Build an{" "}
-                      <Link
-                        url="https://shopify.dev/docs/apps/getting-started/build-app-example"
-                        target="_blank"
-                        removeUnderline
-                      >
-                        {" "}
-                        example app
-                      </Link>{" "}
-                      to get started
-                    </List.Item>
-                    <List.Item>
-                      Explore Shopify’s API with{" "}
-                      <Link
-                        url="https://shopify.dev/docs/apps/tools/graphiql-admin-api"
-                        target="_blank"
-                        removeUnderline
-                      >
-                        GraphiQL
-                      </Link>
-                    </List.Item>
-                  </List>
-                </BlockStack>
-              </Card>
-            </BlockStack>
-          </Layout.Section>
-        </Layout>
-      </BlockStack>
+                  <UpsellRulesTable data={upsellRules} />
+                </>
+              ) : ( */}
+                <Text as="h2" variant="headingMd">
+                  No Discount rules Found!
+                </Text>
+              {/* )} */}
+            
+          </Card>
+        </Layout.Section>
+      </Layout>
     </Page>
   );
 }
