277f95e6b76767221d7a4c3ca57a35ca345ba539
Nur-Alam-Limon <nuralam201425@gmail.com>
2024-02-08 15:50:45 +0600

    Added btn for adding app block

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { json, useLoaderData } from "@remix-run/react";
-import { Page, Card, Layout, Text, DataTable } from "@shopify/polaris";
+import { Page, Card, Layout, Text, DataTable, Button, Box } from "@shopify/polaris";
 import { authenticate } from "../shopify.server";
 import indexStyles from "~/styles/main.css";
 
@@ -20,7 +20,7 @@ export const loader = async ({ request }) => {
     {
       variables: {
         namespace: "green-wallet",
-          key: "discount",
+        key: "discount",
       },
     }
   );
@@ -40,9 +40,7 @@ export default function Index() {
     if (loaderData?.data?.metafield?.value === undefined) {
       console.log(loaderData, "loaderData is undefined");
     } else {
-      const existingRulesData = JSON.parse(
-        loaderData?.data?.metafield?.value
-      );
+      const existingRulesData = JSON.parse(loaderData?.data?.metafield?.value);
       console.log("Loader", existingRulesData);
 
       setdiscountRules(existingRulesData);
@@ -50,15 +48,14 @@ export default function Index() {
     }
   }, [loaderData]);
 
-  return (<ExistingRules discountRules={discountRules} />);
+  return <ExistingRules discountRules={discountRules} />;
 }
 
-function DiscountRulesTable ({ data }) {
+function DiscountRulesTable({ data }) {
   const rows = [];
   data?.forEach((item) => {
-    const MinToken=item.tokenQuantity;
-    const discountCode=item.discountAmount+' %';
-    
+    const MinToken = item.tokenQuantity;
+    const discountCode = item.discountAmount + " %";
 
     rows.push([MinToken, discountCode]);
   });
@@ -66,10 +63,7 @@ function DiscountRulesTable ({ data }) {
   return (
     <DataTable
       columnContentTypes={["text", "text"]}
-      headings={[
-        "Token Quantity",
-        "Discount Percentage",
-      ]}
+      headings={["Token Quantity", "Discount Percentage"]}
       rows={rows}
     />
   );
@@ -90,22 +84,53 @@ function ExistingRules({ discountRules }) {
       // }
     >
       <Layout>
+      <Layout.Section>
+          <Card>
+            <div className="text">
+            <Text as="h2" variant="headingLg">
+              You need to add the app block from customizer to make the discount
+              feature work. Steps to follow:
+              </Text>
+              <ol>
+                <li>Click on the "Add App Block" button which will take you to the customizer.</li>
+                <li>Click on add app block at bottom and add "Green Wallets" app block by selecting the app block if not done yet.</li>
+                <li>Hit save and you are good to go.</li>
+              </ol>
+            </div>
+            <div className="app-block-btn">
+            <Button
+          
+              size="large"
+              variant="primary"
+              onClick={() => {
+                window.open(
+                  `https://admin.shopify.com/store/green-wallet-test/settings/checkout/editor`,
+                  "_blank"
+                );
+              }}
+            >
+              Add App Block
+            </Button>
+            </div>
+          </Card>
+        </Layout.Section>
         <Layout.Section>
           <Card>
             {discountRules?.length > 0 ? (
-                <>
-                  <Text as="h2" variant="headingLg">
-                    Discount Conversion Rate
-                  </Text>
-                  <DiscountRulesTable data={discountRules} />
-                </>
-              ) : (
-            <Text as="h2" variant="headingMd">
-              No Discount conversion rules Found!
-            </Text>
+              <>
+                <Text as="h2" variant="headingLg">
+                  Discount Conversion Rate
+                </Text>
+                <DiscountRulesTable data={discountRules} />
+              </>
+            ) : (
+              <Text as="h2" variant="headingMd">
+                No Discount conversion rules Found!
+              </Text>
             )}
           </Card>
         </Layout.Section>
+        
       </Layout>
     </Page>
   );
