afeac4b1ea0550aa58dbc790ebbbe025de837cbc
Nur-Alam-Limon <nuralam201425@gmail.com>
2024-02-14 14:15:22 +0600

    Added merchant id section and changed UI

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,8 +1,18 @@
-import { useEffect, useState } from "react";
-import { json, useLoaderData } from "@remix-run/react";
-import { Page, Card, Layout, Text, DataTable, Button, Box } from "@shopify/polaris";
+import { useCallback, useEffect, useState } from "react";
+import { json, useLoaderData, useSubmit } from "@remix-run/react";
+import {
+  Page,
+  Card,
+  Layout,
+  Text,
+  DataTable,
+  Button,
+  Box,
+  TextField,
+} from "@shopify/polaris";
 import { authenticate } from "../shopify.server";
 import indexStyles from "~/styles/main.css";
+import axios from "axios";
 
 export const links = () => [{ rel: "stylesheet", href: indexStyles }];
 
@@ -28,7 +38,64 @@ export const loader = async ({ request }) => {
   console.log("Response JSON", responseJson);
   const data = responseJson.data.shop;
   console.log("Data", data);
-  return json({ data });
+
+  const response1 = await admin.graphql(
+    `query ($namespace: String!, $key: String!) {
+        shop {
+        metafield(namespace: $namespace, key: $key) {
+          value
+        }
+      }
+      }`,
+    {
+      variables: {
+        namespace: "green-wallet",
+        key: "merchantid",
+      },
+    }
+  );
+  const responseJson1 = await response1.json();
+  console.log("Response JSON", responseJson1);
+  const data1 = responseJson1.data.shop;
+  console.log("Data", data1);
+
+  return json({ rules: data, id: data1 });
+};
+
+export const action = async ({ request }) => {
+  const { session } = await authenticate.admin(request);
+  let body = await request.formData();
+  let formData = body.get("data");
+  
+  formData = JSON.parse(formData);
+
+  console.log("FormData", formData);
+
+  console.log("Session", session);
+
+  const dataNew = await axios.post(
+    `https://${session.shop}/admin/api/2024-01/metafields.json`,
+    {
+      metafield: {
+        namespace: "green-wallet",
+        key: "merchantid",
+        value: JSON.stringify(formData),
+        type: "json",
+      },
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "X-Shopify-Access-Token": session?.accessToken,
+        "Accept-Encoding": "gzip,deflate,compress",
+      },
+    }
+  );
+
+  console.log("Metafield Data", dataNew.data.metafield);
+  return json({
+    id: dataNew.data,
+  });
 };
 
 export default function Index() {
@@ -36,19 +103,31 @@ export default function Index() {
 
   const [discountRules, setdiscountRules] = useState();
 
+  const [value, setValue] = useState('');
+
   useEffect(() => {
-    if (loaderData?.data?.metafield?.value === undefined) {
+    if (loaderData?.rules?.metafield?.value === undefined) {
       console.log(loaderData, "loaderData is undefined");
     } else {
-      const existingRulesData = JSON.parse(loaderData?.data?.metafield?.value);
+      const existingRulesData = JSON.parse(loaderData?.rules?.metafield?.value);
       console.log("Loader", existingRulesData);
 
       setdiscountRules(existingRulesData);
       // console.log(existingRulesData?.rules);
     }
+    if (loaderData?.id?.metafield?.value === undefined) {
+      console.log(loaderData, "loaderData is undefined");
+
+    } else {
+      const idData = JSON.parse(loaderData?.id?.metafield?.value);
+      console.log("Loader", idData?.id);
+
+      setValue(idData?.id);
+      // console.log(existingRulesData?.rules);
+    }
   }, [loaderData]);
 
-  return <ExistingRules discountRules={discountRules} />;
+  return <ExistingRules discountRules={discountRules} value={value} setValue={setValue} />;
 }
 
 function DiscountRulesTable({ data }) {
@@ -69,48 +148,87 @@ function DiscountRulesTable({ data }) {
   );
 }
 
-function ExistingRules({ discountRules }) {
+function ExistingRules({ discountRules, value, setValue }) {
+  
+  const submit = useSubmit();
+
+
+
+  const handleChange = useCallback(
+    (newValue) => setValue(newValue),
+    [],
+  );
+
+  function handleIDSave(){
+    submit(
+      {
+        data: JSON.stringify({
+          id: value
+        }),
+      },
+      { replace: true, method: "POST" }
+    );
+  }
   return (
     <Page
       title="Green Wallets App"
-      primaryAction={{
-        content: "Add New Discount Conversion Rate",
-        url: "/app/discount-config",
-      }}
-      // secondaryActions={
-      //   <Button variant="primary" url="/app/discount-edit">
-      //     Delete Existing
-      //   </Button>
-      // }
+      
     >
       <Layout>
-      <Layout.Section>
+        <Layout.Section>
           <Card>
             <div className="text">
-            <Text as="h2" variant="headingLg">
-              You need to add the app block from customizer to make the discount
-              feature work. Steps to follow:
+              <Text as="h2" variant="headingLg">
+                You need to add the app block from customizer to make the
+                discount feature work. Steps to follow:
               </Text>
               <ol>
-                <li>Click on the "Add App Block" button which will take you to the customizer.</li>
-                <li>Click on add app block at bottom and add "Green Wallets" app block by selecting the app block if not done yet.</li>
+                <li>
+                  Click on the "Add App Block" button which will take you to the
+                  customizer.
+                </li>
+                <li>
+                  Click on add app block at bottom and add "Green Wallets" app
+                  block by selecting the app block if not done yet.
+                </li>
                 <li>Hit save and you are good to go.</li>
               </ol>
             </div>
             <div className="app-block-btn">
-            <Button
-          
-              size="large"
-              variant="primary"
-              onClick={() => {
-                window.open(
-                  `https://admin.shopify.com/store/green-wallet-test/settings/checkout/editor`,
-                  "_blank"
-                );
-              }}
-            >
-              Add App Block
-            </Button>
+              <Button
+                size="large"
+                variant="primary"
+                onClick={() => {
+                  window.open(
+                    `https://admin.shopify.com/store/green-wallet-test/settings/checkout/editor`,
+                    "_blank"
+                  );
+                }}
+              >
+                Add App Block
+              </Button>
+            </div>
+          </Card>
+        </Layout.Section>
+        <Layout.Section>
+          <Card>
+            <div className="text">
+              <TextField
+                label="Put your Merchant ID here and hit save"
+                value={value}
+                onChange={handleChange}
+                autoComplete="off"
+              />
+            </div>
+            <p style={{ padding: "5px 0 2px", fontWeight:'400'}}>Kindly visit greenwallets.com to find your merchant id</p>
+            <div className="app-block-btn">
+              <Button
+                size="large"
+                variant="primary"
+                onClick={handleIDSave}
+              >
+                Save
+              </Button>
             </div>
           </Card>
         </Layout.Section>
@@ -118,19 +236,39 @@ function ExistingRules({ discountRules }) {
           <Card>
             {discountRules?.length > 0 ? (
               <>
-                <Text as="h2" variant="headingLg">
-                  Discount Conversion Rate
-                </Text>
+                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', paddingTop: '8px'}}>
+                  <Text as="h2" variant="headingLg">
+                    Discount Conversion Rate
+                  </Text>
+                
+                  <Button
+                    size="large"
+                    variant="primary"
+                    url="/app/discount-config"
+                  >
+                    Add New Discount Convertion Rate
+                  </Button>
+                </div>
                 <DiscountRulesTable data={discountRules} />
+                
               </>
             ) : (
-              <Text as="h2" variant="headingMd">
-                No Discount conversion rules Found!
-              </Text>
+              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', paddingTop: '8px'}}>
+                <Text as="h2" variant="headingMd">
+                  No Discount conversion rules Found!
+                </Text>
+                
+                <Button
+                  size="large"
+                  variant="primary"
+                  url="/app/discount-config"
+                >
+                  Add New Discount Convertion Rate
+                </Button>
+              </div>
             )}
           </Card>
         </Layout.Section>
-        
       </Layout>
     </Page>
   );
